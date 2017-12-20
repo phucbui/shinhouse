b8fbb74d5c4839a89a06acc6d22e183465ae9e95
unknown <phucbd.vn@gmail.com>
2017-12-20 20:51:49 +0700

    Add modules

---
+++ config/database.js
@@ -0,0 +1,8 @@
+var db = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'shinhouse'
+  };
+
+module.exports = {db};
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)// + '-' + Date.now()
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('uploadfile');
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.listen(3000);
+
+var config = require('./config/database.js');
+
+var pool = mysql.createPool(config.db);
+
+app.get("/", function(req, res){
+    res.render("user/body");
+});
